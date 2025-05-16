5234b66e8046c83f41d51c6ef94e239ba51fed29
KitawaT <lovepoao01@gmail.com>
2025-05-16 15:12:16 +0700

    add task and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Taskform from "./Taskform";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (text) => {
+    const newTask = { id: Date.now(), text };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-gray-100 flex justify-center items-start pt-10">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center mb-4">Todo App</h1>
+        <Taskform onAdd={addTask} />
+        <ul className="space-y-2 m-2">
+          {tasks.map((task) => (
+            <li 
+            className="p-3 bg-gray-100 border rounded flex justify-between items-center"
+            key={task.id}>{task.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/TaskForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Taskform({onAdd}) {
+  const [task, setTask] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim() === '') return;
+      onAdd(task);
+      setTask('');
+    }
+
+  return (
+
+    <form onSubmit={handleSubmit}  className="flex gap-2 mb-4">
+        <input 
+        type="text" 
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder='Enter your task'
+        
+        />
+        <button 
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer "
+        type='submit'>Add Task</button>
+    </form>
+  )
+}
+
+export default Taskform
